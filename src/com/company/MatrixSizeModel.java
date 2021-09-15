2c0441be2841721c2fc4aa8b33a2b8eb988ff0bd
Guilherme Oliveira <g.p.oliveira@hotmail.com>
2021-09-15 20:39:37 -0300

    Initial commit

---
+++ src/com/company/MatrixSizeModel.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class MatrixSizeModel {
+    private int colLen, rowLen;
+
+
+    public int getRowLen() {
+        return rowLen;
+    }
+
+    public void setRowLen(int rowLen) {
+        this.rowLen = rowLen;
+    }
+
+    public int getColLen() {
+        return colLen;
+    }
+
+    public void setColLen(int colLen) {
+        this.colLen = colLen;
+    }
+}
